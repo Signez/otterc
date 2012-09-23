172333034882c61d4e1f54282e44c25f9eb9f153
michael <michael.schneeberger@epfl.ch>
2012-09-23 16:23:53 +0200

    Implemented TreeSimplifier which replaces for-loops with while-loops

--- whilelang/src/main/scala/whilelang/TreeSimplifier.scala
+++ whilelang/src/main/scala/whilelang/TreeSimplifier.scala
@@ -1,9 +1,31 @@
 package whilelang
 
-object TreeSimplifier {
+/*object TreeSimplifier {
   def apply(stat: Statement): Statement = {
     // Replace all for loops by equivalent trees using while loops...
     println("Tree simplification is not implemented.")
     stat
+  }
+}*/
+
+object TreeSimplifier {
+  def replaceForLoop(body: List[Statement]): List[Statement] = {
+    for {
+      statB <- body
+      //n = statB
+      n = statB match {
+        case For(init, expr, step, body) => new Block(List(init, new While(expr, new Block(List(this(body), step)) )));
+        case _ => this(statB);
+      }
+    } yield n
+  }
+  
+  def apply(stat: Statement): Statement = {
+    stat match {
+      case IfThenElse(expr, then, elze) => new IfThenElse(expr, this(then), this(elze))
+      case While(expr, body) => new While(expr, this(body))
+      case Block(body) => new Block(replaceForLoop(body))
+      case _ => stat;
+    }
   }
 }
