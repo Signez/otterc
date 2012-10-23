037db033b0bf623a57407a1f75aed76844c04a9c
Etienne Kneuss <colder@php.net>
2012-10-23 23:48:29 +0200

    Stubs for Labs 06 - Name analysis

--- toolc/src/main/scala/toolc/TreePrinter.scala
+++ toolc/src/main/scala/toolc/TreePrinter.scala
@@ -3,8 +3,25 @@ package toolc
 object TreePrinter {
   import parser.Trees._
 
-  def apply(t: Tree): String = {
+  /** TreePrinter(tree) will produce the same result as before. */
+  def apply: (Tree=>String) = apply(false)_
+
+  /** TreePrinter.withSymbolIDs(tree) will print the tree with the IDs. */
+  def withSymbolIDs: (Tree=>String) = apply(true)_
+
+  def apply(withSymbolIds: Boolean)(t: Tree): String = {
     /* construct and return the appropriate string ... */
     "Magic magic magic"
+
+    // This adds a symbol id to identifiers, or question marks if it is unset.
+    case id @ Identifier(value) => {
+          if(withSymbolIDs) {
+            try {
+              id.getSymbol.name + "#" + id.getSymbol.id
+            } catch {
+              case e: java.lang.RuntimeException => value + "#??"
+            }
+          } else value
+        }
   }
 }
