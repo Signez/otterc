df506d7baf74c53775b8d680a5e997c4350d6491
Stanislas Signoud <stan@stanisoft.net>
2012-10-31 10:14:23 +0100

    Revert "Trying to revert changes from lab06 stubs."
    This reverts commit b030cb4a910791d17f28ba4545939b49215ff3d6.

--- toolc/src/main/scala/toolc/parser/Trees.scala
+++ toolc/src/main/scala/toolc/parser/Trees.scala
@@ -2,6 +2,22 @@ package toolc
 package parser
 
 object Trees {
+  import analyzer.Symbols._
+
+  /*
+   * Mix the Symbolic trait within the classes representing program parts that
+   * require symbols, such as classes, variable declarations, identifiers,
+   * etc..  The type parameter of the Symbolic Trait denotes the kind of symbol
+   * to attach
+   *
+   * Examples:
+   *    case class ClassDecl(id: Identifier, parent: Option[Identifier], vars: List[VarDecl], methods: List[MethodDecl]) extends Tree with Symbolic[ClassSymbol]
+   *
+   *    case class Identifier(value: String) extends TypeTree with ExprTree with Symbolic[Symbol]
+   *
+   *    case class This extends ExprTree with Symbolic[ClassSymbol]
+   */
+
   sealed trait Tree extends Positional
 
   case class Program(main: MainObject, classes: List[ClassDecl]) extends Tree
