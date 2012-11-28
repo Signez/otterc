21eff695025bf9ec55edcb2790a3a74bf44d96eb
Stanislas Signoud <stan@stanisoft.net>
2012-11-28 10:54:04 +0100

    Fixing problem in main about number of arguments.

--- toolc/src/main/scala/toolc/Main.scala
+++ toolc/src/main/scala/toolc/Main.scala
@@ -2,7 +2,7 @@ package toolc
 
 object Main {
   def main(args: Array[String]) {
-    if (args.length != 1) {
+    if (args.length < 1) {
       Console.err.println("usage: toolc <File.tool>")
       sys.exit(1)
     }
@@ -13,6 +13,7 @@ object Main {
     } else {
       if(!"-d".equals(args(1))) {
         compUnit.fatalError("Unrecognized option: " + args(1))
+        Console.err.println("usage: toolc <File.tool>")
       }
       compUnit.compile(args(2))
     }
