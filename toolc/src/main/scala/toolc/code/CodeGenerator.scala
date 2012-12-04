dcb3b8474bf40573837ab3e60d73142f3660b605
michael <michael.schneeberger@epfl.ch>
2012-12-04 20:24:39 +0100

    changed ASTORE to AStore

--- toolc/src/main/scala/toolc/code/CodeGenerator.scala
+++ toolc/src/main/scala/toolc/code/CodeGenerator.scala
@@ -290,24 +290,29 @@ trait CodeGenerator {
             ch << Label(endLabel)
           }
           case Assignment(id, expr) => {
-            evalExpr(expr)
             id.getSymbol match {
               case vs @ VariableSymbol(_) =>
                 vs.parentSymbol match {
                   case cs @ ClassSymbol(_) =>
+                    evalExpr(expr)
                     ch << PutField(classname, id.value, getTypeSignature(id.getType))
                   case ms @ MethodSymbol(_,_) => 
                     vs.getType match {
                       case TInt =>
+                        evalExpr(expr)
                       	ch << IStore(varMapping(vs))
                       case TBoolean =>
+                        evalExpr(expr)
                         ch << IStore(varMapping(vs))
                       case TString =>
-                        ch << ASTORE 
+                        evalExpr(expr)
+                        ch << AStore(varMapping(vs))
                       case TIntArray =>
-                        ch << ASTORE 
+                        evalExpr(expr)
+                        ch << AStore(varMapping(vs))
                       case TObject(_) =>
-                        ch << ASTORE
+                        evalExpr(expr)
+                        ch << AStore(varMapping(vs))
                       case _ =>
                     }
 
